ac1193b8d834a0b6868285f422c8dd3bca1c9e95
phogtr <phogbtr@gmail.com>
2021-02-10 04:14:25 -0800

    add comments about export

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Countries } from "./Countries/Countries";
 export { default as Country } from "./Country/Country";
 export { default as Detail } from "./Detail/Detail";
 export { default as Weather } from "./Weather/Weather";
+
+// export all the components from components folder
